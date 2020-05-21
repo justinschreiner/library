dbf9b386f82513f46d83191c3964556791fa2638
Justin Schreiner <jschreiner22@gmail.com>
2020-05-21 16:03:28 -0500

    switch list to index

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'book/list'
+  get 'book/index'
   get 'book/new'
   post 'book/create'
   patch 'book/update'
-  get 'book/list'
   get 'book/show'
   get 'book/edit'
   get 'book/delete'
