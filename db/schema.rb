c62af012ec8cd038336dbcb6c17e489c7d29e755
Justin Schreiner <jschreiner22@gmail.com>
2020-05-28 12:08:42 -0500

    changing db migrate file for publishers table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_28_160234) do
+ActiveRecord::Schema.define(version: 2020_05_28_170349) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,8 +24,8 @@ ActiveRecord::Schema.define(version: 2020_05_28_160234) do
   end
 
   create_table "publishers", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.datetime "created_at"
   end
 
   create_table "subjects", force: :cascade do |t|
