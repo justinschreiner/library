71acedf55da355e9c6af3310315ed53f5bcb8bbe
Justin Schreiner <jschreiner22@gmail.com>
2020-06-02 10:34:12 -0500

    adding additional params to copies db migration

--- db/migrate/20200529160454_create_copies.rb
+++ db/migrate/20200529160454_create_copies.rb
@@ -1,8 +1,8 @@
 class CreateCopies < ActiveRecord::Migration[6.0]
   def self.up
     create_table :copies do |t|
-      t.belongs_to :book
-      t.belongs_to :library_location
+      t.belongs_to :book, null: false, foreign_key: true
+      t.belongs_to :library_location, null: false, foreign_key: true
       
       t.column :name, :string
       t.column :checked_out, :boolean
