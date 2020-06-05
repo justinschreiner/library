f0ca0388b70a3898623535111a4f0b64f291f03e
Justin Schreiner <jschreiner22@gmail.com>
2020-06-05 09:05:13 -0500

    removing book-cover relationsip, adding cover photos to gitignore

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,9 +1,10 @@
+# frozen_string_literal: true
+
 class Book < ApplicationRecord
   has_and_belongs_to_many :subjects
   belongs_to :publisher
   has_many :copies
   has_many :library_locations, through: :copies
-  has_one :cover
 
   mount_uploader :cover, CoverUploader
 end
